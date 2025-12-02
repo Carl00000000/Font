ff2df0803af4448ce8b6a7f395c513d187327578
Carl00000000 <60214375+Carl00000000@users.noreply.github.com>
2025-12-02 22:09:38 +0800

    1

--- starting_value.py
+++ starting_value.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def xavier(node_num):
-    return np.random.radn(node_num)/np.sqrt(1.0 /node_num)
+    return np.random.radn(node_num)/np.sqrt(node_num)
 
 def he(node_num)：
-    return np.random.radn(node_num)/np.sqrt(2.0 /node_num)
+    return np.random.radn(node_num)*np.sqrt(2)/np.sqrt(node_num)
