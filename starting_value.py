580844c861e5c41e44598244c43e88054e0fb518
Carl00000000 <60214375+Carl00000000@users.noreply.github.com>
2025-12-14 20:42:39 +0800

    1

--- starting_value.py
+++ starting_value.py
@@ -14,7 +14,11 @@ def set_value():
     value3=xavier(14,14,28)
     value4=xavier(10,10,14)
 
-    data=np.array([value1],[value2],[value3],[value4])
+    data=[]
+    data.extend(value1)
+    data.extend(value2)
+    data.extend(value3)
+    data.extend(value4)
 
 
     with open('mnist.pkl','wb') as file
