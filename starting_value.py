02b3eef12f65aabb6423d30020f516dee14c0a31
Carl00000000 <60214375+Carl00000000@users.noreply.github.com>
2025-12-14 20:31:13 +0800

    1

--- starting_value.py
+++ starting_value.py
@@ -1,17 +1,24 @@
 import numpy as np
 import pickle
 
-def xavier(node_num1,node_num2):
-    return np.random.radn(node_num1，node_num2)/np.sqrt(node_num)
+def xavier(node_num1,node_num2,node_num3):
+    return np.random.randn(node_num1,node_num2)/np.sqrt(node_num3)
 
-def he(node_num)：
-    return np.random.radn(node_num)*np.sqrt(2)/np.sqrt(node_num)
+def he(node_num):
+    return np.random.randn(node_num)*np.sqrt(2)/np.sqrt(node_num)
 
 
 def set_value():
-    
+    value1=xavier(784,784,784)
+    value2=xavier(28,28,28)
+    value3=xavier(14,14,14)
+    value4=xavier(10,10,10)
 
-    data=
+    data=[]
+    data.extend(value1)
+    data.extend(value2)
+    data.extend(value3)
+    data.extend(value4)
 
 
     with open('mnist.pkl','wb') as file
