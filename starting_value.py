8e1ef87a8ad5b3ab73e8794e8ff0777273c9c5b8
Carl00000000 <60214375+Carl00000000@users.noreply.github.com>
2026-02-24 23:46:49 +0800

    1

--- starting_value.py
+++ starting_value.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pickle
+from dataset.mnist import load_mnist
 
 def xavier(node_num1,node_num2,node_num3):
     return np.random.randn(node_num1,node_num2)/np.sqrt(node_num3)
@@ -18,5 +19,20 @@ def set_value():
     
     with open('mnist.pkl','wb') as file
       pickle.dump(data,file)
+
+
+#数据大小为60000
+def import_data():
+    (x_train,t_train),(x_test,t_test)=load_mnist(flatten=True,normalize=False)
+    random_integers = [random.randint(0, 59999) for _ in range(100)]
+    x_1=[]
+    t_1=[]
+    for a in random_integers:
+        x_1.append(x_train[a])
+        t_1.append(t_train[a])
+
+    return x_1,t_1
+    
+
 
 
